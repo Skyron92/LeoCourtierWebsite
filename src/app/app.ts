22dbea5bbd653b44c3ab122925fb9b111616a466
Skyron9 <courtier.leo@gmail.com>
2025-08-12 18:18:06 +0200

    Begin header

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,15 @@
-import { Component, signal } from '@angular/core';
+import {Component, OnInit, signal} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {Header} from '../header/header';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Header],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('LeoCourtierWebsite');
+  protected readonly title = signal('Leo Courtier');
+
+
 }
---
+++ src/header/header.ts
@@ -0,0 +1,64 @@
+import {Component, OnInit} from '@angular/core';
+import {Navigator} from '../navigator/navigator';
+
+@Component({
+  selector: 'app-header',
+  imports: [
+    Navigator
+  ],
+  templateUrl: './header.html',
+  styleUrl: './header.css'
+})
+export class Header implements OnInit {
+  ngOnInit() {
+    this.calculateHeaderColor();
+  }
+
+  #headerOpacity : number = 0.4;
+  #overlayColor : string = '#FFFFFFFF';
+
+  // Calculate header color to create illusion the header is a flat transparent object
+  calculateHeaderColor () {
+    let backgroundGradient = getComputedStyle(document.documentElement).getPropertyValue('--background-gradient');
+    const matches = backgroundGradient.split(',');
+    if(matches) {
+      const col1 = matches[1].trim();
+      const col2 = matches[2].trim();
+      const col3 = matches[3].trim();
+
+      const headerColor1 =this.#calculateColor(col1);
+      const headerColor2 =this.#calculateColor(col2);
+      const headerColor3 =this.#calculateColor(col3);
+
+      const val = `linear-gradient(90deg, ${headerColor1}, ${headerColor2}, ${headerColor3}`;
+      document.documentElement.style.setProperty('--header-gradient', val);
+    }
+  }
+
+  #calculateColor( color:string):string{
+    let bg = this.hexToRgb(color);
+    let ov = this.hexToRgb(this.#overlayColor);
+    if (!bg || !ov) throw new Error("Invalid color format !");
+
+    const r = Math.round(this.#headerOpacity * ov.r + (1 - this.#headerOpacity) * bg.r);
+    const g = Math.round(this.#headerOpacity * ov.g + (1 - this.#headerOpacity) * bg.g);
+    const b = Math.round(this.#headerOpacity * ov.b + (1 - this.#headerOpacity) * bg.b);
+
+    return this.rgbToHex(r, g, b);
+  }
+
+  hexToRgb(hex: string) {
+    hex = hex.replace(/^#/, '');
+    if (hex.length === 3) hex = hex.split('').map(x => x + x).join('');
+    const num = parseInt(hex, 16);
+    return {
+      r: (num >> 16) & 255,
+      g: (num >> 8) & 255,
+      b: num & 255
+    };
+  }
+
+  rgbToHex(r: number, g: number, b: number) {
+    return `#${[r, g, b].map(x => x.toString(16).padStart(2, '0')).join('')}`;
+  }
+}
