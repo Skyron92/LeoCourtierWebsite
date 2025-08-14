0fa2b7d4b1023432ed5d9dd2855a2a155c0395a3
Skyron9 <courtier.leo@gmail.com>
2025-08-14 16:32:59 +0200

    Add projects content

--- src/project/project.ts
+++ src/project/project.ts
@@ -1,4 +1,5 @@
 import {Component, Input, OnInit} from '@angular/core';
+import {DomSanitizer, SafeResourceUrl} from '@angular/platform-browser';
 
 @Component({
   selector: 'app-project',
@@ -8,19 +9,36 @@ import {Component, Input, OnInit} from '@angular/core';
 })
 export class Project implements OnInit {
 @Input() projectName!: string;
-@Input() video?: string;
+@Input() contentType!: string;
+@Input() src?: string;
 @Input() description!: string;
 @Input() iconsUrl?: string[];
+@Input() linkToProject?: string;
+
+srcUrl?: SafeResourceUrl;
 
 firstParagraph : string = '';
 secondParagraph : string = '';
 
-ngOnInit() {
+constructor(private sanitizer: DomSanitizer) {}
+
+  ngOnInit() {
+  if(this.src){
+    const url = this.src;
+    this.srcUrl = this.sanitizer.bypassSecurityTrustResourceUrl(url);
+  }
   if(this.description.includes('|')){
     const split = this.description.split('|');
     this.firstParagraph = split[0];
     this.secondParagraph = split[1];
   }
-  else this.firstParagraph = this.description;
+  else this.firstParagraph = this.description
+}
+
+getIconInfos(index : number) : string[]{
+  if(this.iconsUrl){
+    return this.iconsUrl[index].split('|');
+  }
+  return [];
 }
 }
