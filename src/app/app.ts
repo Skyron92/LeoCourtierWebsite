380436595c7790aeb4176b8f48a08a5a3d29ada2
Skyron <courtier.leo@gmail.com>
2025-08-21 11:09:41 +0200

    remove tailwind

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,12 +2,11 @@ import {Component, signal} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {Header} from '../header/header';
 import {Banner} from '../banner/banner';
-import {ProjectPanel} from '../project-panel/project-panel';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, Header, Banner, ProjectPanel],
+  imports: [RouterOutlet, Header, Banner],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
